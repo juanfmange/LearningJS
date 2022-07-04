6cbdccef4587ce41eab55eaa9a8ebd10f4311fe2
juanfmange <juanfmange@gmail.com>
2022-07-04 01:12:03 -0500

    Modules nodejs

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const axios = require('axios');
+const fs = require('fs').promises;
+const path = require('path');
 
-const main = async() => {
-    let response = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto');
+const main = async () => {
+    let response = await axios.get('https://rickandmortyapi.com/api/character');
     let {
-        data: { name }
+        data: { results }
     } = response;
-    console.log(name);
-}
+    let personajes = results.map((personajes) => {
+        return {
+            id: personajes.id,
+            name: personajes.name,
+            status: personajes.status,
+            species: personajes.species,
+        };
+    }).map((personaje) => Object.values(personaje).join(','))
+        .join('\n');
+    //console.log(path.join(__dirname,'data.csv'));
+    await fs.writeFile(path.join(__dirname, 'data.csv'), personajes)
+//    console.log(personajes);
+};
 
-main()
+main();
