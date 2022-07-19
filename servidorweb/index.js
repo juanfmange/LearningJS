b21b82cbd08460fc58c736088048efe502df5258
Juan Fco. Mange <juanfmange@gmail.com>
2022-07-19 14:38:58 -0500

    web server pt 3

--- servidorweb/index.js
+++ servidorweb/index.js
@@ -1,17 +1,30 @@
 const express = require('express');
-const data = require('./MOCK_DATA.json');
-
+//const data = require('./MOCK_DATA.json');
+const Service = require('./src/service');
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json);
+
 app.get('/', (req,res) => {
     res.json({
         message: 'Lista de usuarios',
-        body: data
-    })
-})
+        body: Service.getUsers(),
+    });
+});
+
+app.post('/', (req, res) => {
+    //let newUser = req.body;
+    let { body: newUser } = req;
+    let user = Service.createUser(newUser);
+    res.status(201).json({
+        message: 'Usuario creado',
+        body: user,
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`servidor escuchando en http://localhost:${PORT}`);
-})
+});
+
