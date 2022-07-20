fa75e98d35d2e610421e93e38c31bde71396a169
juanfmange <juanfmange@gmail.com>
2022-07-20 01:43:08 -0500

    finished 'servidorweb' basics

--- servidorweb/index.js
+++ servidorweb/index.js
@@ -1,18 +1,43 @@
-const express = require('express');
-//const data = require('./MOCK_DATA.json');
-const Service = require('./src/service');
+const express = require("express");
+const Service = require("./src/service");
+const {request} = require("express");
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json);
+app.use(express.json());
 
-app.get('/', (req,res) => {
+app.get("/", (req,res) => {
     res.json({
         message: 'Lista de usuarios',
         body: Service.getUsers(),
     });
 });
+
+app.get("/:id", (req,res) => {
+    //let id = req.params;
+    let { params : {id}} = req;
+    let user = Service.getUser(id);
+    console.log(id);
+    res.json({
+        message: `usuario ${id}`,
+        body: user
+    });
+});
+
+app.put('/:id',(req,res)=>{
+
+})
+
+app.delete('/:id',(req,res)=>{
+    let { params : {id}} = req;
+    let user = Service.deleateUser(id);
+    console.log(id);
+    res.json({
+        message: `usuario ${id} eliminado`,
+    })
+
+})
 
 app.post('/', (req, res) => {
     //let newUser = req.body;
--- servidorweb/src/service.js
+++ servidorweb/src/service.js
@@ -2,6 +2,21 @@ const data = require('./MOCK_DATA.json');
 
 module.exports = {
     getUsers: () => data,
+    getUser: (id) => {
+        let identificador = Number(id);
+        let user = data.filter((person)=>person.id === identificador)[0];
+        return user;
+    },
+    updateUser: (id) => {
+        let identificador = Number(id);
+        let user = data.filter((person)=>person.id === identificador)[0];
+        return user;
+    },
+    deleateUser: (id) => {
+        let identificador = Number(id);
+        let user = data.filter((person)=>person.id === identificador)[0];
+        return user;
+    },
     createUser: (dataUser) => {
         let newUser = {
             id: data.length + 1,
