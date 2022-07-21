18d4a8311267955c58af76f9f1f7c3c8901b96d5
juanfmange <juanfmange@gmail.com>
2022-07-21 02:32:30 -0500

    dependencias instaladas para el proyecto

---
+++ project/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const debug = require('debug')('app:main');
+
+const { Config } = require('./src/config/index')
+
+const app = express();
+
+app.use(express.json);
+
+
+
+
+app.listen(Config.port, () => {
+    debug(`servidor escuchando en el puerto ${Config.port}`);
+});
