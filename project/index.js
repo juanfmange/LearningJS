6faea237f5873864a4c6366cd0e66ab7aa76fd96
juanfmange <juanfmange@gmail.com>
2022-07-24 01:27:36 -0500

    ready to deploy

--- project/index.js
+++ project/index.js
@@ -4,16 +4,19 @@ const express = require('express');
 const { Config } = require('./src/config/index');
 const { Productsapi } = require('./src/products/index');
 const { Usersapi } = require('./src/users/index');
+const { IndexAPI,NotFoundAPI } = require("./src/index/index")
 const app = express();
 
 app.use(express.json);
+
+IndexAPI(app);
 Usersapi(app);
 Productsapi(app);
-
+NotFoundAPI(app);
 
 app.listen(Config.port, () => {
     //debug(`servidor escuchando en el puerto ${Config.port}`);
-    console.log(`Servidor escuchando en el puerto ${Config.port}`)
+    console.log(`Servidor escuchando en el puerto ${Config.port}`);
 });
 
 
---
+++ project/src/index/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const createErrors = require('http-errors');
+const { Response } = require('../common/response');
+module.exports.IndexAPI = (app) => {
+    const router = express.Router();
+    router.get("/", (req, res) => {
+        const menu = {
+            products: `https://${req.headers.host}/api/products`,
+            users: `https://${req.headers.host}/api/users`,
+        }   
+        Response.sucess(res, 200, "API inventario", menu)
+    })
+    app.use("/", router);
+}
+
+module.exports.NotFoundAPI = (app) => {
+    const router = express.Router();
+    router.all("*", (req, res) => {
+        Response.error(res, new createErrors.NotFound());
+    });
+
+    app.use("/",router);
+}
